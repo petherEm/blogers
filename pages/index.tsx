5f66eb21755c564942e1c15766ff71f1b3bd354f
Piotr Maciejewski <pm@piotrs-macbook-pro.home>
2022-03-13 16:43:12 +0100

    minor update

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,7 @@ export default function Home({ posts }: Props) {
             is a place to write, read, and connect.
           </h1>
           <h2>
-            It's easy and free to post your thinking on any topic and connect
+            It is easy and free to post your thinking on any topic and connect
             with millions of readers.
           </h2>
         </div>
